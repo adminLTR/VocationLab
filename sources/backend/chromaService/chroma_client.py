efbef40de37fe7e700a50054e9ed0ec172e8f281
Luis LT <105554066+LTprograms@users.noreply.github.com>
2025-05-04 22:37:32 -0500

    frontend react

--- sources/backend/chromaService/chroma_client.py
+++
@@ -1,7 +0,0 @@
-# chroma_client.py
-import chromadb
-# from chromadb.config import Settings
-
-persist_directory = "./chroma_db"
-client = chromadb.PersistentClient(path=persist_directory)
-collection = client.get_or_create_collection(name="careers")
