15a4858ec4725b54d6bd7b755bdf42d709bde8df
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-05-14 17:09:02 -0500

    .

--- sources/backend/location/admin.py
+++ sources/backend/location/admin.py
@@ -1,19 +1,48 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
 @admin.register(Region)
 class RegionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'region')
+    search_fields = ('name', 'region__name')
+    list_filter = ('region',)
+
 
 @admin.register(District)
 class DistrictAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'get_ciudad', 'get_region')
+    search_fields = ('name', 'city__name', 'city__region__name')
+    list_filter = ('city',)
 
-@admin.register(Adress)
-class AdressAdmin(admin.ModelAdmin):
-    pass
+    @admin.display(description="Ciudad")
+    def get_ciudad(self, obj):
+        return obj.city.name
+    
+    @admin.display(description="Región")
+    def get_region(self, obj):
+        return obj.city.region
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'address', 'get_district', 'get_city', 'get_region', 'latitude', 'longitude')
+    search_fields = ('address', 'district__name', 'district__city__name', 'district__city__region__name')
+    list_filter = ('district', 'district__city')
+
+    @admin.display(description="Distrito")
+    def get_district(self, obj):
+        return obj.district.name
+    
+    @admin.display(description="Ciudad")
+    def get_city(self, obj):
+        return obj.district.city.name
+
+    @admin.display(description="Región")
+    def get_region(self, obj):
+        return obj.district.city.region
--- sources/backend/location/models.py
+++ sources/backend/location/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Region(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nombre")
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = "Región"
         verbose_name_plural = "Regiones"
@@ -12,22 +15,32 @@ class City(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nombre")
     region = models.ForeignKey(Region, on_delete=models.CASCADE, verbose_name="Región")
 
+    def __str__(self):
+        return f"{self.region.name}/{self.name}"
+
     class Meta:
         verbose_name = "Ciudad"
         verbose_name_plural = "Ciudades"
 
 class District(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nombre")
-    city = models.ForeignKey(Region, on_delete=models.CASCADE, verbose_name="Ciudad")
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name="Ciudad")
+
+    def __str__(self):
+        return f"{self.city}/{self.name}"
 
     class Meta:
         verbose_name = "Distrito"
         verbose_name_plural = "Distritos"
 
-class Adress(models.Model):
-    adress = models.CharField(max_length=500, verbose_name="Dirección")
+class Address(models.Model):
+    address = models.CharField(max_length=500, verbose_name="Dirección")
     longitude = models.FloatField(null=True, blank=True, verbose_name="Longitud")
     latitude = models.FloatField(null=True, blank=True, verbose_name="Latitude")
+    district = models.ForeignKey(District, on_delete=models.CASCADE, verbose_name="Distrito")
+
+    def __str__(self):
+        return f"{self.address} - {self.district}"
     
     class Meta:
         verbose_name = "Dirección"
