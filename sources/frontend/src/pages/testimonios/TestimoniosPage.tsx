50f96faae555b7f74575e7e3234ce5f980cdf9bf
Luis LT <105554066+LTprograms@users.noreply.github.com>
2025-05-15 20:19:02 -0500

    testimonios

--- sources/frontend/src/pages/testimonios/TestimoniosPage.tsx
+++ sources/frontend/src/pages/testimonios/TestimoniosPage.tsx
@@ -1,14 +1,42 @@
 import Title from "../../components/Title"
+import Slider from "./components/Slider"
+import CommentBox from "./components/CommentBox"
 
 export default function TestimoniosPage() {
     return <div>
-        <Title>
-            <p className="text-center">
-                <span>Testimonios de</span>
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-main-500 to-secondary-600">
-                    nuestros usuarios
-                </span>
-            </p>
-        </Title>
+        <div className="w-fit m-auto">
+            <Title>
+                <p className="text-center mb-3">
+                    <span>Testimonios de</span>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-main-500 to-secondary-600">
+                        nuestros usuarios
+                    </span>
+                </p>
+                <div className="w-full h-1 bg-gradient-to-r from-main-700 via-secondary-400 to-secondary-600"></div>
+            </Title>
+        </div>
+        <div className="my-10">
+            <Slider data={[
+                {
+                    profilePhotoUrl: 'user1.png',
+                    comment: 'Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, pharetra netus cras augue placerat cum duis commodo, magna mollis molestie ultricies odio aenean rhoncus. Gravida mauris malesuada litora velit tincidunt montes blandit platea ornare.',
+                    username: 'Juanito del Campo'
+                },
+                {
+                    profilePhotoUrl: 'user2.png',
+                    comment: 'Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, pharetra netus cras augue placerat cum duis commodo, magna mollis molestie ultricies odio aenean rhoncus. Gravida mauris malesuada litora velit tincidunt montes blandit platea ornare.',
+                    username: 'Juanito del Campo'
+                },
+                {
+                    profilePhotoUrl: 'user3.png',
+                    comment: 'Lorem ipsum dolor sit amet consectetur adipiscing elit tempus, pharetra netus cras augue placerat cum duis commodo, magna mollis molestie ultricies odio aenean rhoncus. Gravida mauris malesuada litora velit tincidunt montes blandit platea ornare.',
+                    username: 'Juanito del Campo'
+                },
+            ]}/>
+        </div>
+        <div className="my-10">
+            <h3 className="font-inter mb-4 text-[25px]">Deja tu testimonio</h3>
+            <CommentBox/>
+        </div>
     </div>
 }
