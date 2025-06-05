48f75d600a8bd37fce6b5b68b7421afb196b6dd1
AJ <135352588+AsthriPardave@users.noreply.github.com>
2025-06-05 15:04:09 -0500

    asd

---
+++ sources/frontend/src/pages/nosotros/components/Card.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+interface CardProbs {
+    profilePhotoUrl: string;
+    name: string;
+    position: string;
+}
+
+export default function Card({profilePhotoUrl, name, position}: CardProbs){
+    return <div className="bg-red-50 w-[200px] h-[210px] py-4 px-1 rounded-4xl">
+        <div className="pb-5">
+            <img className="block m-auto mb-2 rounded-full" src={profilePhotoUrl} alt="" />
+            <p className="font-inter text-[18px] font-semibold">{name}</p>
+            <p className="font-inter text-[16px] font-medium text-[#7B8C9F]">{position}</p>
+        </div>
+        <div className="flex gap-7 items-center justify-center">
+            <Link to="#" style={{fontSize: '20px'}}>
+                <i className="fa-brands fa-github"></i>
+            </Link>
+            <Link to="#" style={{fontSize: '20px'}}>
+                <i className="fa-brands fa-linkedin-in"></i>
+            </Link>
+            <Link to="#" style={{fontSize: '20px'}}>
+                <i className="fa-brands fa-x-twitter"></i>
+            </Link>
+        </div>
+    </div>
+}
