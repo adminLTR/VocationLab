47b2cb41ff200c71034628667a780b77be8c1135
AJ <135352588+AsthriPardave@users.noreply.github.com>
2025-06-05 15:04:09 -0500

    asd

--- sources/frontend/src/pages/nosotros/NosotrosPage.tsx
+++ sources/frontend/src/pages/nosotros/NosotrosPage.tsx
@@ -1,11 +1,42 @@
 import Title from "../../components/Title";
-import { Link } from "react-router-dom";
+import Card from "./components/Card";
 
 export default function NosotrosPage() {
-
+    const data = [
+        {
+            profilePhotoUrl:"/yo_mejorado.png",
+            name:"Hugo Alvarez",
+            position:"Word Senior"
+        },
+        {
+            profilePhotoUrl:"/yo_mejorado.png",
+            name:"Ever Avedaño",
+            position:"Figma Senior"
+        },
+        {
+            profilePhotoUrl:"/yo_mejorado.png",
+            name:"Luis La Torre",
+            position:"JS Senior"
+        },
+        {
+            profilePhotoUrl:"/yo_mejorado.png",
+            name:"Bruno Chochoca",
+            position:"Word Senior"
+        },
+        {
+            profilePhotoUrl:"/yo_mejorado.png",
+            name:"Giacomo Madrid",
+            position:"Word Senior"
+        },
+        {
+            profilePhotoUrl:"/yo_mejorado.png",
+            name:"Laura Mendoza",
+            position:"Word Senior"
+        },
+    ]
     return <div>
         <div className="text-center">
-            <div className="w-fit m-auto p-0">
+            <div className="w-fit m-auto py-2">
                 <Title>
                     <span className="text-[60px]">Nuestro </span>
                     <span className="text-transparent text-[60px] bg-clip-text bg-gradient-to-r from-main-500 to-secondary-600">
@@ -13,26 +44,18 @@ export default function NosotrosPage() {
                     </span>
                 </Title>
             </div>
-            <p className="text-[#BDCFE2] text-[40px] font-medula">Apasionados. Creativos. Alcohólicos.</p>
-            <div >
-                <div className="bg-red-100 w-fit m-auto py-4 px-10 rounded-3xl">
-                    <div className="pb-5">
-                        <img className="block m-auto mb-2 rounded-full" src="/yo_mejorado.png" alt="" />
-                        <p className="font-inter text-[18px] font-semibold">Hugo Álvarez</p>
-                        <p className="font-inter text-[16px] font-medium text-[#7B8C9F]">Word Senior</p>
-                    </div>
-                    <div className="flex gap-5 items-center justify-center">
-                        <Link to="#" style={{fontSize: '20px'}}>
-                            <i className="fa-brands fa-github"></i>
-                        </Link>
-                        <Link to="#" style={{fontSize: '20px'}}>
-                            <i className="fa-brands fa-linkedin-in"></i>
-                        </Link>
-                        <Link to="#" style={{fontSize: '20px'}}>
-                            <i className="fa-brands fa-x-twitter"></i>
-                        </Link>
-                    </div>
-                </div>
+            <p className="text-[#BDCFE2] text-[40px] font-medula mt-1 mb-4">Apasionados. Creativos. Alcohólicos.</p>
+            <div className="flex gap-10 justify-center flex-wrap">
+                {data.map((usuario, index) => {
+                    return <Card 
+                        {...usuario}
+                        key={index}
+                    />
+                })}
+            </div>
+            <div className="font-inter font-medium text-[18px] text-[#92A1B0]">
+                <p>VocationLab nació al reflexionar sobre el proceso de preparación preuniversitaria que siguió cada integrante de nuestro equipo. Nos dimos cuenta que al escoger una carrera universitaria, no se cuenta con una herramienta que cuente con la efectividad del servicio de un profesional en guía vocacional, en un entorno digital y de forma gratuita. </p>
+                <p>Este proyecto es de código abierto, por lo que contamos con las sugerencias provenientes de la comunidad académica para brindar una mayor efectividad en nuestro test.</p>
             </div>
         </div>
     </div>
