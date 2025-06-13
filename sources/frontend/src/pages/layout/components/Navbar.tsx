2993718dd246df912ed8ef2bff4c7d86ed861ce0
Luis LT <105554066+LTprograms@users.noreply.github.com>
2025-06-13 00:08:31 -0500

    header responsivo

--- sources/frontend/src/pages/layout/components/Navbar.tsx
+++ sources/frontend/src/pages/layout/components/Navbar.tsx
@@ -6,8 +6,9 @@ interface NavLinkProps {
   children: ReactNode;
   active?:boolean
 }
-function NavLink({children, to, active=false} : NavLinkProps) {
-    return <Link to={to} className={`transition font-bold px-2 py-2 hover:text-black ${active?'border-b-4 text-black':'border-b-0 text-gray-350'} border-b-main-200`}>
+function NavLink({children, to, active=false} : NavLinkProps) { 
+    return <Link to={to} 
+    className={`transition flex items-center gap-2 font-bold px-2 py-2 hover:text-black ${active?'lg:border-b-4 text-black':'border-b-0 text-gray-350'} border-b-main-200`}>
         {children}
     </Link>
 }
@@ -15,14 +16,24 @@ function NavLink({children, to, active=false} : NavLinkProps) {
 export default function Navbar() {
     const location = useLocation();
 
-    return <nav className="flex justify-center font-inter space-x-4 pt-1 px-6 rounded-3xl mt-2 md:mt-0 bg-white">
+    return <nav className="flex justify-center font-inter space-x-4 pt-4 lg:py-0 px-6 rounded-3xl mt-2 md:mt-0 bg-white
+    flex-col lg:flex-row text-[14px] min-w-[224px]">
         <NavLink to="/" active={location.pathname === '/'}>
+            <span className="inline-block lg:hidden">
+                <i className="fa-solid fa-house"></i>                
+            </span>
             Inicio
         </NavLink>
         <NavLink to="/testimonios" active={location.pathname === '/testimonios'}>
+            <span className="inline-block lg:hidden">
+                <i className="fa-solid fa-comment"></i>
+            </span>
             Testimonios
         </NavLink>
         <NavLink to="/nosotros" active={location.pathname === '/nosotros'}>
+            <span className="inline-block lg:hidden">
+                <i className="fa-solid fa-users"></i>                             
+            </span>
             Nosotros
         </NavLink>
     </nav>
--- sources/frontend/src/pages/layout/components/SocialMediaLinks.tsx
+++ sources/frontend/src/pages/layout/components/SocialMediaLinks.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 }
 function Button({children, onClick = ()=>{}} : ButtonProps) {
     return <button onClick={onClick}
-    className="text-center rounded-3xl bg-black text-white px-4 py-2 font-bold cursor-pointer hover:bg-gray-800 text-[14px]">
+    className="text-center rounded-3xl bg-black text-white text-[14px] px-4 py-2 font-bold cursor-pointer hover:bg-gray-800">
         {children}
     </button>
 }
@@ -17,11 +17,15 @@ interface SocialMediaLinksProps {
   setShowModal: (x:boolean) => void
 }
 export default function SocialMediaLinks({setShowModal}:SocialMediaLinksProps) {
-    return <div className="flex space-x-4 mt-2 md:mt-0 items-center">
+    return <div className="flex space-x-4 py-2 md:mt-0 items-start lg:items-center px-6 flex-col lg:flex-row">
         <Button onClick={()=>setShowModal(true)}>
-            Login / Register
+            Login / Sign Up
         </Button>
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center flex-wrap pt-4 lg:pt-0">
+            <p className="block lg:hidden w-full">Siguenos:</p>
+            <Link to="#" className="text-[20px] bg-white px-2 py-1 rounded-lg hover:bg-gray-50 transition-all">
+                <i className="fa-brands fa-linkedin-in"></i>
+            </Link>
             <Link to="#" className="text-[20px] bg-white px-2 py-1 rounded-lg hover:bg-gray-50 transition-all">
                 <i className="fa-brands fa-github"></i>
             </Link>
