0d4eb6bf2248bf5c5b4598106a1dc906e104fc6d
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-06-13 20:04:47 -0500

    testimonios .

--- sources/frontend/src/pages/testimonios/TestimoniosPage.tsx
+++ sources/frontend/src/pages/testimonios/TestimoniosPage.tsx
@@ -3,20 +3,16 @@ import Slider from "./components/Slider"
 
 export default function TestimoniosPage() {
     
-    return <div>
-        <div className="w-fit m-auto">
-            <Title>
-                <p className="text-center mb-3 text-[60px] w-full">
-                    <span>Nuestros</span> 
-                    &nbsp;
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-main-500 to-secondary-600">
-                         usuarios
-                    </span>
-                    &nbsp;
-                    <span>opinan</span>
-                </p>
-            </Title>
-        </div>
+    return <div className="w-full">
+        <h1 className="font-sf font-semibold leading-[0.8] w-fit text-center mb-3 text-[60px]">
+            <span>Nuestros</span> 
+            &nbsp;
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-main-500 to-secondary-600">
+                    usuarios
+            </span>
+            &nbsp;
+            <span>opinan</span>
+        </h1>
         <p className="text-center mb-8">
             Nuestra mision es brindar una herramienta efectiva y una experiencia satisfactoria a nuestros usuarios
         </p>
