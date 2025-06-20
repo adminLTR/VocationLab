e9550f3d88b6ee4f98fe8318b6edf2b855f73c5f
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-06-19 20:36:43 -0500

    chat

---
+++ sources/frontend/src/pages/chat/components/UserMessage.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+interface UserMessageProps {
+    children: ReactNode,
+    user: boolean
+}
+export default function UserMessage({children, user}: UserMessageProps) {
+    return <div className={`flex ${user?'justify-end' : 'justify-start'}`}>
+        <div className={`max-w-[300px] py-2 px-3 rounded-xl ${user?'bg-[#1DD1CB]':'bg-[#8379F5]'} text-white`}>
+            {children}
+        </div>
+    </div>
+}
