79d3d5629dac8839720aada12921f09a4e71fc6d
Luis LT <105554066+LTprograms@users.noreply.github.com>
2025-06-20 00:52:04 -0500

    admin messages backend

--- sources/backend/accounts/models.py
+++ sources/backend/accounts/models.py
@@ -6,3 +6,6 @@ class UserProfile(models.Model):
     profile_picture = models.ImageField(null=True, blank=True, upload_to='users')
     birth_date = models.DateField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
