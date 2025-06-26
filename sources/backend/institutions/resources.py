ada285858efb514e1a524745f864735e0a11476a
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-06-26 18:58:22 -0500

    import export completo para universidades

--- sources/backend/institutions/resources.py
+++ sources/backend/institutions/resources.py
@@ -4,42 +4,45 @@ from import_export.widgets import ForeignKeyWidget
 from .models import Career, Area, Institution
 from location.models import Region, City, District, Address
 from import_export.fields import Field
+import openpyxl
 
 class InstitutionResource(resources.ModelResource):
-    nro = Field(column_name='Nro')
-    program = Field(column_name='Programa')
-    universidad = Field(column_name='Universidad')
-    departamento = Field(column_name='Departamento')
-    provincia = Field(column_name='Provincia')
-    distrito = Field(column_name='Distrito')
-    direccion = Field(column_name='Dirección del local')
-    tipo = Field(column_name='Tipo de gestión')
-    nivel = Field(column_name='Nivel académico')
-    modalidad = Field(column_name='Modalidad de estudio')
+    # nro = Field(column_name='Nro')
+    # program = Field(column_name='Programa')
+    # universidad = Field(column_name='Universidad')
+    # tipo = Field(column_name='Tipo de gestión')
+    # nivel = Field(column_name='Nivel académico')
+    # modalidad = Field(column_name='Modalidad de estudio')
+    # departamento = Field(column_name='Departamento')
+    # provincia = Field(column_name='Provincia')
+    # distrito = Field(column_name='Distrito')
+    # direccion = Field(column_name='Dirección del local')
 
-    def before_import_row(self, row, row_number=None, **kwargs):
+    def before_import_row(self, row, **kwargs):
         if row['Nivel académico'].strip().lower() != 'carrera profesional':
-            raise Exception("Ignorado: No es Carrera Profesional")
-        region, _ = Region.objects.get_or_create(name=row['Departamento'])
-        city, _ = City.objects.get_or_create(name=row['Provincia'], region=region)
-        district, _ = District.objects.get_or_create(name=row['Distrito'], city=city)
-        address, _ = Address.objects.get_or_create(address=row['Dirección del local'], district=district)
+            return super().before_import_row(row, **kwargs)
+        try:
+            region, _ = Region.objects.get_or_create(name=row['Departamento'])
+            city, _ = City.objects.get_or_create(name=row['Provincia'], region=region)
+            district, _ = District.objects.get_or_create(name=row['Distrito'], city=city)
+            address, _ = Address.objects.get_or_create(address=row['Dirección del local'], district=district)
 
-        # Career
-        career, _ = Career.objects.get_or_create(name=row['Programa'])
+            # Career
+            career, _ = Career.objects.get_or_create(name=row['Programa'])
 
-        # Institution
-        institution, _ = Institution.objects.get_or_create(name=row['Universidad'], defaults={
-            'address': address,
-            'institution_type' : Institution.UNIVERSITY,
-            'management_type': Institution.PUBLIC if row['Tipo de gestión'] == 'Público' else Institution.PRIVATE
-        })
+            # Institution
+            institution, _ = Institution.objects.get_or_create(name=row['Universidad'], defaults={
+                'address': address,
+                'institution_type' : Institution.UNIVERSITY,
+                'management_type': Institution.PUBLIC if row['Tipo de gestión'] == 'Público' else Institution.PRIVATE
+            })
 
-        institution.careers.add(career)
-
+            institution.careers.add(career)
+        except:
+            return super().before_import_row(row, **kwargs)
         # Previene que la importación intente duplicar
-        row['id'] = institution.id
-        return super().before_import_row(row, row_number, **kwargs)
+        # row['id'] = institution.id
+        return super().before_import_row(row, **kwargs)
 
     class Meta:
         model = Institution
