49e3896f4ea552dc6d3d52b006280422107bf08a
Nesodev <ever.avendano@unmsm.edu.pe>
2025-07-02 11:23:48 -0500

    add bg testimonials view

--- sources/frontend/src/pages/testimonios/TestimoniosPage.tsx
+++ sources/frontend/src/pages/testimonios/TestimoniosPage.tsx
@@ -1,8 +1,14 @@
 import Slider from "./components/Slider"
+import bgSvg from "../../assets/bg-svg/bg-testimonials.svg"
 
 export default function TestimoniosPage() {
     
-    return <div className="w-full h-full">
+    return <div className="relative w-full h-full">
+    <img
+        src={bgSvg}
+        alt="bg"
+        className="w-full h-auto absolute top-0 left-0 -z-10"
+    />
         <div className="py-10 w-full md:w-10/12 mx-auto flex flex-col h-full">
             <h1 className="font-sf font-semibold leading-[0.8] text-center
             text-[30px] md:text-[40px] lg:text-[60px]">
