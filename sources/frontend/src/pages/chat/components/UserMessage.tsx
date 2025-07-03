5c32dc56df0a81ef4f63ae2dedd2c04d2778b1d2
LTprograms <105554066+LTprograms@users.noreply.github.com>
2025-07-02 20:08:29 -0500

    respuestas y categorias en backend

--- sources/frontend/src/pages/chat/components/UserMessage.tsx
+++ sources/frontend/src/pages/chat/components/UserMessage.tsx
@@ -6,7 +6,7 @@ interface UserMessageProps {
 }
 export default function UserMessage({children, user}: UserMessageProps) {
     return <div className={`flex ${user?'justify-end' : 'justify-start'}`}>
-        <div className={`max-w-[300px] py-2 px-3 rounded-xl ${user?'bg-[#1DD1CB]':'bg-[#8379F5]'} text-white`}>
+        <div className={`max-w-[400px] py-2 px-3 rounded-xl ${user?'bg-[#1DD1CB]':'bg-[#8379F5]'} text-white`}>
             {children}
         </div>
     </div>
