028dcb435458f11177b8097d37b5a441cb4ba793
Nesodev <ever.avendano@unmsm.edu.pe>
2025-07-03 10:44:40 -0500

    change env name vars

--- sources/backend/backend/settings.py
+++ sources/backend/backend/settings.py
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("POSTGRES_DB"),
+        'NAME': os.getenv("POSTGRES_DBNAME"),
         'USER': os.getenv("POSTGRES_USER"),
-        'PASSWORD': os.getenv("POSTGRES_PASS"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"),
         'PORT': os.getenv("POSTGRES_PORT")
     }
