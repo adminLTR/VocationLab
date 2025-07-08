177f03206a1217c9a0be5af1fee64b08fd08753c
Nesodev <ever.avendano@unmsm.edu.pe>
2025-07-08 12:42:34 -0500

    edit width user message

--- sources/frontend/src/pages/chat/components/UserMessage.tsx
+++ sources/frontend/src/pages/chat/components/UserMessage.tsx
@@ -7,7 +7,7 @@ interface UserMessageProps {
 }
 export default function UserMessage({children, user, isLoading}: UserMessageProps) {
     return <div className={`flex ${user?'justify-end' : 'justify-start'}`}>
-        <div className={`max-w-[400px] py-2 px-3 rounded-xl ${user?'bg-[#1DD1CB]':'bg-[#8379F5]'} text-white`}>
+        <div className={`max-w-[550px] py-2 px-3 rounded-xl ${user?'bg-[#1DD1CB]':'bg-[#8379F5]'} text-white`}>
             {isLoading ? (
                 <div className="flex items-center gap-1">
                     <div className="w-2 h-2 bg-white rounded-full animate-bounce"></div>
